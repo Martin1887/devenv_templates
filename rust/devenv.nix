548043a4472b1780cc2ebffd9bd1e5ed566b0350
Martin1887 <mpozo@protonmail.com>
2025-04-23 10:18:17 +0200

    fix: vscode-lldb new package name and path in $PATH

--- rust/devenv.nix
+++ rust/devenv.nix
@@ -1,10 +1,13 @@
 { pkgs, lib, config, inputs, ... }:
 
 {
+  enterShell = ''
+    export PATH="$PATH:${pkgs-unstable.vscode-extensions.vadimcn.vscode-lldb}/share/vscode/extensions/vadimcn.vscode-lldb/adapter"
+  '';
   languages.rust.enable = true;
   languages.rust.channel = "stable";
   packages = [
-    pkgs.vscode-extensions.vadimcn.vscode-lldb.adapter
+    pkgs.vscode-extensions.vadimcn.vscode-lldb
     pkgs.taplo
   ];
 }
